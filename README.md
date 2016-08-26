2da6c662af7cea1ce2da50c3b663c5efe0a429dc
JT Olds <hello@jtolds.com>
2016-08-26 17:58:18 -0600

    better docs

--- README.md
+++ README.md
@@ -4,6 +4,68 @@ A plugin for http://github.com/spacemonkeygo/monkit that supports Zipkin.
 
 See the docs at http://godoc.org/gopkg.in/spacemonkeygo/monkit-zipkin.v2
 
+## Example usage
+
+Your main method gets set up something like this:
+
+```go
+package main
+
+import (
+  "net/http"
+
+  "gopkg.in/spacemonkeygo/monkit.v2"
+  "gopkg.in/spacemonkeygo/monkit.v2/environment"
+  "gopkg.in/spacemonkeygo/monkit.v2/present"
+  "gopkg.in/spacemonkeygo/monkit-zipkin.v2"
+)
+
+func main() {
+  environment.Register(monkit.Default)
+  go http.ListenAndServe("localhost:9000", present.HTTP(monkit.Default))
+  collector, err := zipkin.NewScribeCollector("zipkin.whatever:9042")
+  if err != nil {
+    panic(err)
+  }
+  zipkin.RegisterZipkin(monkit.Default, collector, zipkin.Options{})
+
+  ...
+}
+```
+
+Once you've done that, you need to make sure your HTTP handlers pull Zipkin
+`Context` info from the `Request`. That's easy to do with
+`zipkin.ContextWrapper`.
+
+```go
+func HandleRequest(ctx context.Context, w http.ResponseWriter,
+  r *http.Request) {
+  defer mon.Task()(&ctx)(nil)
+
+  ... whatever
+}
+
+func DoTheThing(ctx context.Context) (err error) {
+  defer mon.Task()(&ctx)(&err)
+  return http.Serve(listener, zipkin.ContextWrapper(
+    zipkin.ContextHTTPHandlerFunc(HandleRequest)))
+}
+```
+
+Last, your outbound HTTP requests need to pass through Context info:
+
+```go
+func MakeRequest(ctx context.Context) (err error) {
+  defer mon.Task()(&ctx)(&err)
+  req, err := http.NewRequest(...)
+  if err != nil {
+    return err
+  }
+  resp, err := zipkin.TraceRequest(ctx, http.DefaultClient, req)
+  ...
+}
+```
+
 ## License
 
 Copyright (C) 2016 Space Monkey, Inc.
--- doc.go
+++ doc.go
@@ -12,4 +12,61 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* Package zipkin provides a monkit plugin for sending traces to Zipkin.
+
+Example usage
+
+Your main method gets set up something like this:
+
+  package main
+
+  import (
+    "net/http"
+
+    "gopkg.in/spacemonkeygo/monkit.v2"
+    "gopkg.in/spacemonkeygo/monkit.v2/environment"
+    "gopkg.in/spacemonkeygo/monkit.v2/present"
+    "gopkg.in/spacemonkeygo/monkit-zipkin.v2"
+  )
+
+  func main() {
+    environment.Register(monkit.Default)
+    go http.ListenAndServe("localhost:9000", present.HTTP(monkit.Default))
+    collector, err := zipkin.NewScribeCollector("zipkin.whatever:9042")
+    if err != nil {
+      panic(err)
+    }
+    zipkin.RegisterZipkin(monkit.Default, collector, zipkin.Options{})
+
+    ...
+  }
+
+Once you've done that, you need to make sure your HTTP handlers pull Zipkin
+Context info from the Request. That's easy to do with zipkin.ContextWrapper.
+
+  func HandleRequest(ctx context.Context, w http.ResponseWriter,
+    r *http.Request) {
+    defer mon.Task()(&ctx)(nil)
+
+    ... whatever
+  }
+
+  func DoTheThing(ctx context.Context) (err error) {
+    defer mon.Task()(&ctx)(&err)
+    return http.Serve(listener, zipkin.ContextWrapper(
+      zipkin.ContextHTTPHandlerFunc(HandleRequest)))
+  }
+
+Last, your outbound HTTP requests need to pass through Context info:
+
+  func MakeRequest(ctx context.Context) (err error) {
+    defer mon.Task()(&ctx)(&err)
+    req, err := http.NewRequest(...)
+    if err != nil {
+      return err
+    }
+    resp, err := zipkin.TraceRequest(ctx, http.DefaultClient, req)
+    ...
+  }
+*/
 package zipkin // import "gopkg.in/spacemonkeygo/monkit-zipkin.v2"
